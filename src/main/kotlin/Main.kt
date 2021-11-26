8087c3c1d0bfd8a2e03dd1fded3d7690b7531e24
Danil Medvedev <dvede123@yandex.ru>
2021-11-27 00:03:38 +0300

    Output of detailed weather information

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,23 +2,27 @@ import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
+import kotlin.math.floor
 
 fun main(args: Array<String>) {
     when (args.size) {
         0 -> {
-            println("Arguments not found")
+            println("Нет аргументов!")
             return
         }
         1 -> {
-            val urlAddress = "https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&appid=0f21bd02afdc24c032045afd167ff588&units=metric"
+            val urlAddress = "https://api.openweathermap.org/data/2.5/weather?q=${args[0]}" +
+                    "&appid=0f21bd02afdc24c032045afd167ff588&units=metric&lang=ru"
             val output = getWeatherInfo(urlAddress)
             if (output.isNotBlank()) {
                 val obj = JSONObject(output)
-                println("Температура: " + obj.getJSONObject("main").getDouble("temp"))
+                println(generalInfo(obj))
+                println(temperatureInfo(obj))
+                println(windInfo(obj))
             }
         }
         else -> {
-            println("Too much arguments")
+            println("Слишком много аргументов!")
             return
         }
     }
@@ -34,4 +38,35 @@ private fun getWeatherInfo(urlAddress: String): String {
         println("Такой город не найден!")
     }
     return result
+}
+
+private fun generalInfo(obj: JSONObject) = buildString {
+    append("-----Общая информация-----\n")
+    append("Город: ${obj.getString("name")}\n")
+    append("Широта: ${obj.getJSONObject("coord").getDouble("lon")}\n")
+    append("Долгота: ${obj.getJSONObject("coord").getDouble("lat")}\n")
+    append("Погода: ${obj.getJSONArray("weather").getJSONObject(0).getString("description")}\n")
+}
+
+private fun temperatureInfo(obj: JSONObject) = buildString {
+    append("-----Информация о температуре-----\n")
+    append("Температура: ${obj.getJSONObject("main").getDouble("temp")}\n")
+    append("Ощущается: ${obj.getJSONObject("main").getDouble("feels_like")}\n")
+    append("Минимальная: ${obj.getJSONObject("main").getDouble("temp_min")}\n")
+    append("Максимальная: ${obj.getJSONObject("main").getDouble("temp_max")}\n")
+    append("Давление: ${obj.getJSONObject("main").getDouble("pressure")}\n")
+    append("Влажность: ${obj.getJSONObject("main").getDouble("humidity")}\n")
+}
+
+private fun windInfo(obj: JSONObject) = buildString {
+    append("-----Информация о ветре-----\n")
+    append("Скорость ветра: ${obj.getJSONObject("wind").getDouble("speed")}\n")
+    append("Направление ветра: ${getWindDirection(obj.getJSONObject("wind").getDouble("deg"))}\n")
+}
+
+private fun getWindDirection(degrees: Double): String {
+    val section = floor(degrees / 45)
+    val arr = listOf("Северный","Северо-Западный","Западный","Юго-Западный",
+        "Южный","Юго-Восточный", "Восточный", "Северо-Восточный")
+    return arr[(section % 8).toInt()]
 }
