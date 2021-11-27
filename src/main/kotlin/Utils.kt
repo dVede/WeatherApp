79960fb90b54ed18efc824d74e3c1a29574145a8
Danil Medvedev <dvede123@yandex.ru>
2021-11-27 20:51:45 +0300

    Error messages added to some wrong cases (no internet connection, empty fields, and incorrect city name)

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,15 +1,24 @@
 import org.json.JSONObject
 import java.io.BufferedReader
+import java.io.FileNotFoundException
 import java.io.InputStreamReader
 import java.net.URL
+import java.net.UnknownHostException
 import kotlin.math.floor
 
 const val URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=0f21bd02afdc24c032045afd167ff588" +
         "&units=metric&lang=ru"
-const val CELSIUS_SIGN = " °С"
-const val HUMIDITY_SIGN = " %"
-const val PRESSURE_SIGN = " мм рт. ст."
-const val SPEED_SIGN = " м/c"
+const val CELSIUS_UNIT = "°С"
+const val HUMIDITY_UNIT = "%%"
+const val PRESSURE_UNIT = "мм рт. ст."
+const val SPEED_UNIT = "м/c"
+const val DEFAULT_CITY = "Москва"
+const val INTERNET_ERROR_MSG = "No internet connection"
+const val CITY_ERROR_MSG = "City with this name was not found"
+const val EMPTY_ERROR_MSG = "Text field is empty"
+const val ICON_PATH = "/icons/%s.png"
+private val codesList = listOf("01d","01n","02d","02n","03d","03n","04d","04n","09d","09n",
+    "10d","10n","11d","11n","13d","13n","50d","50n")
 
 fun getWindDirection(degrees: Double): String {
     val section = floor(degrees / 45)
@@ -24,31 +33,14 @@ fun getJSONFromUrl(urlAddress: String) : JSONObject {
         val urlConnection = URL(urlAddress).openConnection()
         val stream = InputStreamReader(urlConnection.getInputStream())
         result = stream.buffered().use(BufferedReader::readText)
-    } catch (e: Exception) {
-        throw IllegalArgumentException("City not found")
+    } catch (e: UnknownHostException) {
+        throw UnknownHostException(INTERNET_ERROR_MSG)
+    } catch (e: FileNotFoundException) {
+        throw FileNotFoundException(CITY_ERROR_MSG)
     }
     return JSONObject(result)
 }
 
-fun getIcon(code: String): String =
-    when (code) {
-        "01d" -> "/icons/01d.png"
-        "01n" -> "/icons/01n.png"
-        "02d" -> "/icons/02d.png"
-        "02n" -> "/icons/02n.png"
-        "03d" -> "/icons/03d.png"
-        "03n" -> "/icons/03n.png"
-        "04n" -> "/icons/04n.png"
-        "04d" -> "/icons/04d.png"
-        "09n" -> "/icons/09n.png"
-        "09d" -> "/icons/09d.png"
-        "10n" -> "/icons/10n.png"
-        "10d" -> "/icons/10d.png"
-        "11n" -> "/icons/11n.png"
-        "11d" -> "/icons/11d.png"
-        "13n" -> "/icons/13n.png"
-        "13d" -> "/icons/13d.png"
-        "50n" -> "/icons/50n.png"
-        "50d" -> "/icons/50d.png"
-        else -> "/icons/unknown.png"
-    }
+fun getIcon(code: String): String = if (codesList.contains(code)) String.format(ICON_PATH, code)
+else String.format(ICON_PATH, "unknown")
+
